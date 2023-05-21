85121fb47ba2805a28e26294ac1e70f2fb7ad9c0
Bhargey Shingala <bhargeys@gmail.com>
2023-05-21 23:20:48 +0530

    Done with Backend fro FER

---
+++ Backend/api.py
@@ -0,0 +1,41 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import pickle
+from deepface import DeepFace
+import cv2
+import matplotlib.pyplot as plt
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Update this with the appropriate origin or origins
+    allow_methods=["*"],  # Update this with the appropriate HTTP methods
+    allow_headers=["*"],  # Update this with the appropriate headers
+)
+
+model = DeepFace
+
+
+class ImageData(BaseModel):
+    image: str
+
+
+@app.get('/index')
+def getprediction():
+    return {'emotion': 'Happy'}
+
+
+@app.post('/predict')
+def detectEmotion(data: ImageData):
+    image = data.image
+    prediction = model.analyze(image)
+    print(prediction[0])
+
+    return {'emotion': prediction}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='127.0.0.1', port=8000)
